e7fed0a67925f8e403b1fa1300bbb8e2c0cc0f3d
Dereje1 <Dereje1>
2017-10-04 09:50:22 -0400

    removed unnecessary files/folders

--- app.js
+++ app.js
@@ -10,9 +10,7 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +20,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// DEFINES THE MAIN ENTRY POINT
+app.get('*', function(req, res){
+   res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+  });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
