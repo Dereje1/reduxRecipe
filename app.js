7a0c0ee6151c2c1ba22431ded1d91ec66deab986
Dereje1 <Dereje1>
2017-10-05 09:50:30 -0400

    added Mongo,access thru mongoose, test api get/post/delete/put with ARC

--- app.js
+++ app.js
@@ -5,12 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+//var index = require('./routes/index');
+//var users = require('./routes/users');
 
 var app = express();
-
-
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -19,6 +17,74 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+//APIs
+var mongoose = require('mongoose')
+mongoose.connect("mongodb://localhost:27017/recipebook")
+var Recipes = require('./models/recipes')
+
+//Posts New Recipes//
+app.post('/recipes', function(req,res){
+  var recipeAdd = req.body;
+  //console.log(recipe)
+  Recipes.create(recipeAdd,function(err,recipes){
+    if(err){
+      throw err;
+    }
+    res.json(recipes)
+  })
+
+})
+
+//GETS Recipes from db
+app.get('/recipes',function(req,res){
+  Recipes.find(function(err,recipes){
+    if(err){
+      throw err;
+    }
+    res.json(recipes)
+  })
+
+})
+
+//Deletes Recipes from db
+app.delete('/recipes/:_id', function(req, res){
+   var query = {_id: req.params._id};
+   console.log("Delete Request ", query )
+   Recipes.remove(query, function(err, recipe){
+     if(err){
+
+     throw err;
+     }
+     res.json(recipe);
+   })
+});
+
+//update Recipes from db
+app.put('/recipes/:_id', function(req, res){
+   var recipeToUpdate = req.body;
+   var recipeID = req.params._id;
+   // if the field doesn't exist $set will set a new field
+   //if you leave any of the update fields out in the actual update
+   // then it will set that property to null in the database therefore it is best to
+   //just specify the property you want to update so the others remain intact, in this case
+   //just the ingredients
+   var update = {
+       '$set':{
+       ingredients:recipeToUpdate.ingredients
+       }
+    };
+   // When true returns the updated document
+   var options = {new: true};
+   Recipes.findOneAndUpdate(recipeID, update, options, function(err, recipe){
+       if(err){
+         throw err;
+       }
+       res.json(recipe);
+   })
+})
+//END APIs
 
 // DEFINES THE MAIN ENTRY POINT
 app.get('*', function(req, res){
@@ -35,12 +101,13 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
+  //console.log(req.body)
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.end('error');
 });
 
 module.exports = app;
